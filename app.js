8f0075046114e28a88915a0c10beaf530ed4d4f2
Ayush23Dash <ayushshanker23@gmail.com>
2020-04-16 11:35:22 +0530

    Added correct port

--- app.js
+++ app.js
@@ -183,6 +183,10 @@ app.post("/", (req, res) => {
     }
   });
 });
-app.listen(process.env.PORT||3000, (req, res) => {
-  console.log("Server is running on port 3000");
-});
+// app.listen(process.env.PORT||3000, (req, res) => {
+//   console.log("Server is running on port 3000");
+// });
+app.listen(process.env.PORT || 3000,() => {
+console.log("Server started successfully");
+}
+);
